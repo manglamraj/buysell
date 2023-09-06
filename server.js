ee30b045af7b26a51759e372ae270645447015a1
Yash mallik <yashmallik12@gmail.com>
2023-09-06 17:58:47 +0530

    adding body parser

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const dotenv = require("dotenv");
 
 dotenv.config();
 const app = express();
-port = 5000;
+port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("Server working successfully ");
